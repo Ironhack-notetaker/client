cdcce4f5d9de21dafd776aecc62b742a91e887a3
Luis Flores <leinad15.df@gmail.com>
2018-05-21 16:20:21 -0400

    views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { NoteService } from './services/note.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Note taker';
+
+  constructor(private myService: NoteService) {}
+
+  formInfo: any = {username: "", password: ""};
+
+  user: any;
+  error: any;
+  title = 'Note Taker';
+
+  login() {
+    this.myService.login(this.formInfo)
+    .subscribe(
+      (user) => this.user = user,
+      (err) => this.error = err
+    );
+  }
+
+  signup() {
+    this.myService.signup(this.formInfo)
+    .subscribe(
+      (user) => {
+        this.user = user;
+      },
+      (err) => this.error = err
+    );  
+  }
+
+  logout() {
+    this.myService.logout()
+    .subscribe(
+      (user) => {
+        this.user = null
+        this.formInfo = {};
+      },
+      (err) => this.error = err
+    );  
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
+import { NoteService } from './services/note.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [NoteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
